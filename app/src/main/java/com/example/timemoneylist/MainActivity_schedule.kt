745b234f7340949e3ac2c8aef43266cfebdb025b
Jaen522 <qlql8510@naver.com>
2023-10-31 11:09:46 +0900

    23/10/31 - 하나로 합침

--- app/src/main/java/com/example/timemoneylist/MainActivity_schedule.kt
+++ app/src/main/java/com/example/timemoneylist/MainActivity_schedule.kt
@@ -3,18 +3,12 @@ package com.example.timemoneylist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.timemoneylist.databinding.ActivityMainScheduleBinding
-import com.example.timemoneylist.ui.theme.Input_schedule
 
 class MainActivity_schedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainScheduleBinding.inflate(layoutInflater)
 
-        //fragment를 배치하는 코드
-        supportFragmentManager.beginTransaction().run{
-            //replace(binding.schedule)
-            commit()
-        }
 
         setContentView(binding.root)
 
